36c10390b7e8ec0597a4a88872b140681cbdc8db
Tatiana <tanya18082000@mail.ru>
2024-01-29 17:49:28 +0300

    add: spinner in articles

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
-    <div class="text-center">
-      <div class="spinner-border text-light" role="status">
+    <div class="text-center p-lg-5">
+      <div class="spinner-border text-primary" role="status">
         <span class="visually-hidden">Загрузка...</span>
       </div>
     </div>
--- src/pages/articles/articles.tsx
+++ src/pages/articles/articles.tsx
@@ -1,19 +1,22 @@
-import { createResource } from "solid-js";
+import { Suspense, createResource } from "solid-js";
 import ListRows from "../../components/list-rows/list-rows";
 
 import BaseLayout from "../../layouts/base/base-layout";
 import { fetchArticles } from "../../api/api";
 import ControlPanel from "../../components/control-panel/control-panel";
 import Caption from "../../components/caption/caption";
+import Spinner from "../../components/spinner/spinner";
 
 export default function ArticlesPage() {
   const [data] = createResource(true, fetchArticles);
   return (
     <BaseLayout>
       <div class="container-lg px-9 text-center">
-        <Caption mainText={"Статьи"} extraText={"за 2020-2024 гг"}/>
+        <Caption mainText={"Статьи"} extraText={"за 2020-2024 гг"} />
         <ControlPanel />
-        <ListRows articles={data()} />
+        <Suspense fallback={<Spinner />}>
+          <ListRows articles={data()} />
+        </Suspense>
       </div>
     </BaseLayout>
   );
